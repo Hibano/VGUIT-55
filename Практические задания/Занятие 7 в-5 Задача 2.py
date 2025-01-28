f09b08931b3ed3336b3e356f8732c18637ed2302
Hibano <21hibano@gmail.com>
2025-01-28 10:37:11 +0300

    Update Занятие 7 в-5 Задача 2.py

--- Практические задания/Занятие 7 в-5 Задача 2.py
+++ Практические задания/Занятие 7 в-5 Задача 2.py
@@ -1,13 +1,1 @@
-arr = []
 
-for _ in range(10):
-
-      arr.append(int(input('Введите числа в массив:')))
-
-
-
-for i in range(9):
-
-      if arr[i] < 0 and arr[i + j1] < 0:
-
-            print("Пара чисел:", arr[i], arr[i + 1])
